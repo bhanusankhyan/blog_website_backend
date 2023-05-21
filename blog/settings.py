ea56564eaf707a88a19056236ed6dbe63fe8f668
Bhanu <bhanusankhyan77@gmail.com>
2023-05-21 11:30:27 +0530

    database connection

--- blog/settings.py
+++ blog/settings.py
@@ -5,6 +5,6 @@ from pymongo import MongoClient
 # mongo_uri = 'mongodb://' + \
 #     os.environ["MONGO_HOST"] + ':' + os.environ["MONGO_PORT"]
 
-mongo_uri = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+mongo_uri = "mongodb+srv://blog_website:dsChOS0iNKlkJULg@cluster0.o855bjo.mongodb.net/"
 DB_NAME = 'blogdb'
 db = MongoClient(mongo_uri)[DB_NAME]
