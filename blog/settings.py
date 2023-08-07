0b45aba38ea2cbab165cc694295d2f78fbd277a0
unknown <bhanusankhyan77@gmail.com>
2023-08-07 11:58:01 +0530

    uri string

--- blog/settings.py
+++ blog/settings.py
@@ -5,6 +5,8 @@ from pymongo import MongoClient
 # mongo_uri = 'mongodb://' + \
 #     os.environ["MONGO_HOST"] + ':' + os.environ["MONGO_PORT"]
 
-mongo_uri = "mongodb+srv://blog_website:dsChOS0iNKlkJULg@cluster0.o855bjo.mongodb.net/"
+# mongo_uri = "mongodb+srv://blog_website:dsChOS0iNKlkJULg@cluster0.o855bjo.mongodb.net/"
+mongo_uri = "mongodb+srv://blogdb:ERpfVe9gUptDwt9C@cluster0.zz6st7n.mongodb.net/"
+# mongo_uri = "mongodb://localhost:27017"
 DB_NAME = 'blogdb'
 db = MongoClient(mongo_uri)[DB_NAME]
